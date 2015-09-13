d54e9d20db130aeeb4015db60d4d485397f072cb
Eduardo Ferro Aldama <eduardo.ferro.aldama@gmail.com>
2015-09-13 15:58:41 +0200

    New entinties

--- gokafka/decisionsession.go
+++ gokafka/decisionsession.go
@@ -1,4 +1,16 @@
+package main
+
+type Participant struct {
+}
+
+type Circle struct {
+     Id string
+     Participants []Participant
+}
+
 type DecisionSession struct {
+     Name string
+     Circles []Circle
 }
 
 func NewDecisionSession() *DecisionSession {
